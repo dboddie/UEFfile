e5babafb300040761d9a580fab952ae8d4155234
David Boddie <david@boddie.org.uk>
2017-05-24 16:53:52 +0200

    Made the filter frequencies (--f1, --f2) configurable. Made pulse widths (--w1200, --w2400) configurable. Made the zero count (--zc) configurable. Improved the filtering algorithm to handle both double and single spikes. Improved time reporting in the output. Added a WAV import function. Made error output simpler to read. Added an option to try and track the 2400 Hz carrier tone (--adapt), though this makes time reporting inaccurate.

--- recordUEF.py
+++ recordUEF.py
@@ -123,7 +123,8 @@ class Block(UEFfile.UEFfile):
             print >>sys.stderr, repr(self.name), hex(self.load_addr), hex(self.exec_addr), hex(self.number), self.length
         
         if self.length > 256:
-            raise ValueError("Invalid block length (%i)." % self.length)
+            raise ValueError("Invalid block length (%i) at %s." % (
+                self.length, hms(self.T)))
         
         self.flags = gen.next()
         header += chr(self.flags)
@@ -137,8 +138,8 @@ class Block(UEFfile.UEFfile):
             print >>sys.stderr, self.flags, self.next, hex(self.header_crc)
         
         if self.crc(header) != self.header_crc:
-            raise ValueError("Invalid block header (%x != %x)." % (
-                self.crc(header), self.header_crc))
+            raise ValueError("Invalid block header (%x != %x) at %s." % (
+                self.crc(header), self.header_crc, hms(self.T)))
         
         self.block = "".join(map(lambda x: chr(gen.next()), range(self.length)))
         self.block_crc = sum(map(lambda x: gen.next() << x, range(0, 16, 8)))
@@ -147,8 +148,8 @@ class Block(UEFfile.UEFfile):
             print >>sys.stderr, repr(self.block), len(self.block)
         
         if self.crc(self.block) != self.block_crc:
-            raise ValueError("Invalid block (%x != %x)." % (
-                self.crc(self.block), self.block_crc))
+            raise ValueError("Invalid block (%x != %x) at %s." % (
+                self.crc(self.block), self.block_crc, hms(self.T)))
         
         if debug:
             print >>sys.stderr, repr(self.block)
@@ -162,16 +163,24 @@ class Block(UEFfile.UEFfile):
 
 class Reader:
 
-    def __init__(self, format, step, sample_rate, boost_factor, filter_,
-                       quiet, debug):
+    def __init__(self, format, step, sample_rate, boost_factor, f1, f2,
+                       width_1200, width_2400, zero_count,
+                       filter_, adapt_frequency, quiet, debug):
     
         self.format = format
         self.step = step
         self.sample_rate = sample_rate
         self.boost_factor = boost_factor
         self.filter_ = filter_
+        self.adapt_frequency = adapt_frequency
         self.quiet = quiet
         self.debug = debug
+        
+        self.f1 = f1
+        self.f2 = f2
+        self.width_1200 = width_1200
+        self.width_2400 = width_2400
+        self.zero_count = self.sample_rate/zero_count
         
         self.dt = 1.0/sample_rate
         self.T = 0
@@ -212,7 +221,11 @@ class Reader:
                 self.bits = self.bits >> 1
                 self.shift += 1
                 if self.debug:
-                    print >>sys.stderr, "0", tc, hex(self.bits)
+                    print >>sys.stderr, "bit 0", tc, hex(self.bits), 
+                    if self.shift == 8:
+                        print >>sys.stderr, "<-", hex(self.bits), repr(chr(self.bits))
+                    else:
+                        print >>sys.stderr
             
             elif self.state == "ready":
             
@@ -248,7 +261,12 @@ class Reader:
                     self.shift += 1
                     self.cycles = 0
                     if self.debug:
-                        print >>sys.stderr, "1", tc, hex(self.bits)
+                        print >>sys.stderr, "bit 1", tc, hex(self.bits),
+                        if self.shift == 8:
+                            print >>sys.stderr, "<-", hex(self.bits), repr(chr(self.bits))
+                        else:
+                            print >>sys.stderr
+                
                 elif self.debug:
                     print >>sys.stderr, "-", tc, hex(self.bits)
         
@@ -258,6 +276,11 @@ class Reader:
                     self.state = "ready"
                     if self.debug:
                         print >>sys.stderr, tc, self.state
+            
+            if self.state == "ready" and self.adapt_frequency:
+                f = 1/(tc - self.last_tc)
+                target_dt = self.dt * (f/2400)
+                self.dt = 0.5*(self.dt + target_dt)
         else:
             self.state = "waiting"
             return
@@ -282,12 +305,12 @@ class Reader:
         self.ymax = 0
         
         # Low-pass filter constants
-        resonant_f1 = 1200.0
+        resonant_f1 = self.f1
         R1 = 1000
         C1 = 1.0/(2 * math.pi * resonant_f1 * R1)
         
         # High-pass filter constants
-        resonant_f2 = 1200.0
+        resonant_f2 = self.f2
         R2 = 1000
         C2 = 1.0/(2 * math.pi * resonant_f2 * R2)
         
@@ -307,11 +330,8 @@ class Reader:
         start_t = None
         end_t = None
         
-        self.width_1200 = 1/3200.0
-        self.width_2400 = 1/7000.0
-        
         floor_count = 0
-        zero_count = self.sample_rate/6200
+        zero_count = self.zero_count
         mean_count = self.sample_rate/3900
         
         if self.debug:
@@ -329,33 +349,60 @@ class Reader:
             if self.stop_time != None and self.T > self.stop_time:
                 break
             
-            values = struct.unpack(format, sample)
+            try:
+                values = struct.unpack(format, sample)
+            except struct.error:
+                raise StopIteration
+            
             value = values[0]
             
             if self.filter_:
             
                 buf.append(value)
                 
-                if len(buf) < 3:
-                    self.T += dt
+                if len(buf) < 4:
+                    self.T += self.dt
                     continue
                 
                 #print "", buf[0], buf[1], buf[2]
                 
+                # Check for a point that crosses the zero line out of sequence.
                 if buf[0] * buf[1] < 0 and buf[0] * buf[2] > 0:
-                    if self.debug:
-                        print >>sys.stderr, " spike at", self.T, buf[1], "->", (buf[0] + buf[2])/2.0
-                    buf[1] = (buf[0] + buf[2])/2.0
+                
+                    # Check for another crossing.
+                    if buf[1] * buf[3] > 0:
+                        # Interpolate the two middle points.
+                        if self.debug:
+                            print >>sys.stderr, " double spike at", hms(self.T), buf[0], buf[1], buf[2], buf[3], "->", (buf[1] + buf[3])/2.0
+                        buf[1] = ((2 * buf[0]) + buf[3])/3.0
+                        buf[2] = (buf[0] + (2 * buf[3]))/3.0
+                    else:
+                        # Interpolate the second point.
+                        if self.debug:
+                            print >>sys.stderr, " single spike at", hms(self.T), buf[0], buf[1], buf[2], buf[3], "->", (buf[0] + buf[2])/2.0
+                        buf[1] = (buf[0] + buf[2])/2.0
+                
+                else:
+                    b0 = buf[1] - buf[0]
+                    b1 = buf[2] - buf[1]
+                    b2 = buf[3] - buf[2]
+                    if b0 * b1 < 0 and b0 * b2 > 0:
+                    
+                        # Interpolate the second point.
+                        if self.debug:
+                            print >>sys.stderr, " smoothing", hms(self.T), buf[0], buf[1], buf[2], buf[3], "->", (buf[0] + buf[2])/2.0
+                        buf[2] = (buf[1] + buf[3])/2.0
                 
                 value = buf.pop(0)
+                #print >>sys.stderr, value
             
             mean = ((mean * (mean_count - 1)) + value)/mean_count
             
             Vapp = (value - mean) * self.boost_factor/8.0
             # Apply the low-pass filter.
-            Vc1, i1 = self.V(Vc1, Vapp, R1, C1, dt)
+            Vc1, i1 = self.V(Vc1, Vapp, R1, C1, self.dt)
             # Apply the high-pass filter to the output of the low-pass filter.
-            Vc2, i2 = self.V(Vc2, Vc1, R2, C2, dt)
+            Vc2, i2 = self.V(Vc2, Vc1, R2, C2, self.dt)
             
             y = max(0.0, min(i2 * R2, 1.0))
             dy = y - old_y
@@ -405,7 +452,7 @@ class Reader:
                 sys.stdout.write("\r%s " % hms(self.T))
             
             old_y = y
-            self.T += dt
+            self.T += self.dt
     
     def read_block(self, audio_f):
     
@@ -422,6 +469,36 @@ class Reader:
                     raise
 
 
+def check_wav(f, mono, sample_rate, sample_size):
+
+    if not f.read(4) == "RIFF":
+        f.seek(0, 0)
+        return mono, sample_rate, sample_size
+    
+    f.seek(4, 1)
+    
+    if f.read(4) != "WAVE":
+        raise IOError("Not a WAV file I understand.")
+    
+    if f.read(4) != "fmt ":
+        raise IOError("Not a WAV file I understand.")
+    
+    header_size = struct.unpack("<I", f.read(4))[0]
+    
+    f.seek(4, 1)
+    sample_rate = struct.unpack("<I", f.read(4))[0]
+    f.seek(6, 1)
+    
+    sample_size = struct.unpack("<H", f.read(2))[0]
+    
+    if f.read(4) != "data":
+        raise IOError("Not a WAV file I understand.")
+    
+    f.seek(4, 1)
+    
+    return mono, sample_rate, sample_size
+
+
 if __name__ == "__main__":
 
     program_name, args = sys.argv[0], sys.argv[1:]
@@ -435,10 +512,16 @@ if __name__ == "__main__":
     debug = find_option(args, "--debug", 0)
     right = find_option(args, "--right", 0)
     boost, boost_factor = find_option(args, "--boost", 1)
+    use_f1, f1 = find_option(args, "--f1", 1)
+    use_f2, f2 = find_option(args, "--f2", 1)
+    set_width_1200, width_1200 = find_option(args, "--w1200", 1)
+    set_width_2400, width_2400 = find_option(args, "--w2400", 1)
+    set_zero_count, zero_count = find_option(args, "--zc", 1)
     filter_ = find_option(args, "--filter", 0)
+    adapt_frequency = find_option(args, "--adapt", 0)
     quiet = find_option(args, "--quiet", 0)
     
-    if len(args) != 2 or not s or not r:
+    if len(args) != 2:
         sys.stderr.write("Usage: %s [--rate <sample rate in Hz>] [--mono] [--unsigned] [--size <sample size in bits>] [--start <time in seconds>] [--stop <time in seconds>] [--boost <factor>] [--filter] <audio file> <UEF file>\n" % program_name)
         sys.exit(1)
     
@@ -449,6 +532,14 @@ if __name__ == "__main__":
         audio_f = sys.stdin
     else:
         audio_f = open(audio_file, "rb")
+        mono, wav_sample_rate, sample_size = check_wav(audio_f, mono, sample_rate, sample_size)
+        if not r:
+            sample_rate = wav_sample_rate
+        s = r = True
+    
+    if not s or not r:
+        sys.stderr.write("Usage: %s [--rate <sample rate in Hz>] [--mono] [--unsigned] [--size <sample size in bits>] [--start <time in seconds>] [--stop <time in seconds>] [--boost <factor>] [--filter] <audio file> <UEF file>\n" % program_name)
+        sys.exit(1)
     
     dt = 1.0/float(sample_rate)
     
@@ -468,6 +559,31 @@ if __name__ == "__main__":
     if not boost_factor:
         boost_factor = 1.0
     
+    if use_f1:
+        f1 = float(f1)
+    else:
+        f1 = 1200.0
+    
+    if use_f2:
+        f2 = float(f2)
+    else:
+        f2 = 1200.0
+    
+    if set_width_1200:
+        width_1200 = 1/float(width_1200)
+    else:
+        width_1200 = 1/3200.0
+        
+    if set_width_2400:
+        width_2400 = 1/float(width_2400)
+    else:
+        width_2400 = 1/7000.0
+    
+    if set_zero_count:
+        zero_count = int(zero_count)
+    else:
+        zero_count = 6200
+    
     step = int(sample_size/8)
     
     if right:
@@ -484,7 +600,8 @@ if __name__ == "__main__":
     
     format = "<" + format
     reader = Reader(format, step, int(sample_rate), float(boost_factor),
-                    filter_, quiet, debug)
+                    f1, f2, width_1200, width_2400, zero_count,
+                    filter_, adapt_frequency, quiet, debug)
     reader.start_at(float(start_time))
     print "Seeking to", float(start_time)
     audio_f.seek(step * float(start_time) * int(sample_rate), 1)
@@ -496,23 +613,17 @@ if __name__ == "__main__":
     data = []
     blocks = []
     
-    if False:
-        for byte in reader.read_byte(audio_f):
-        
-            data.append(byte)
-            #print reader.T, hex(byte)
-            if int(reader.T) > last_T:
-                last_T = int(reader.T)
-                #sys.stdout.write("\r%02i:%02i" % (last_T/60, last_T % 60))
-                #sys.stdout.flush()
-                #print ">", last_T
-    else:
+    try:
         for block in reader.read_block(audio_f):
             if not debug:
-                print hms(block.T), block.name, hex(block.load_addr), hex(block.exec_addr), hex(block.number), block.length
+                print hms(block.T), block.name, hex(block.load_addr), hex(block.exec_addr), hex(block.number), block.length, hex(block.flags)
             else:
-                print "%.2f (%s)" % (block.T, hms(block.T)), block.name, hex(block.load_addr), hex(block.exec_addr), hex(block.number), block.length
+                print "%.2f (%s)" % (block.T, hms(block.T)), block.name, hex(block.load_addr), hex(block.exec_addr), hex(block.number), block.length, hex(block.flags)
             blocks.append(block)
+    except:
+        exc_type, exc, tb = sys.exc_info()
+        sys.stderr.write(str(exc) + "\n")
+        sys.exit(1)
     
     if not debug:
         print
